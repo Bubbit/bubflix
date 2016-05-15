0422b6441d2a2827e2c68cb57a2bb1f13fa75b82
Remco Gubbels <Remco@guanabana-macbookpro.local>
2016-05-15 21:17:55 +0200

    Added first steps of downloading torrents for movies

--- src/app/bubflix/bubflix-movie-card/bubflix-movie-card.component.ts
+++ src/app/bubflix/bubflix-movie-card/bubflix-movie-card.component.ts
@@ -1,13 +1,18 @@
 import {Component, Input} from '@angular/core';
+import {NgClass} from '@angular/common';
+
 import {Movie} from '../bubflix-movie/movie';
+import {TorrentService} from '../../services/torrentService';
 
 @Component({
     selector: 'movie-card',
+    directives: [NgClass],
+    providers: [TorrentService],
     template: `
     <div class="panel panel-default">
         <div class="panel-heading">{{movie.Title}}</div>
         <div class="panel-body">
-           <img [src]="movie.Poster">
+           <img [src]="movie.Poster" class="img-responsive" alt="{{movie.Title}}">
         </div> 
         <ul class="list-group">
             <li class="list-group-item">Metascore: {{movie.Metascore}}</li>
@@ -15,11 +20,34 @@ import {Movie} from '../bubflix-movie/movie';
             <li class="list-group-item">Genre: {{movie.Genre}}</li>
             <li class="list-group-item">Year: {{movie.Year}}</li>
             <li class="list-group-item">Plot: {{movie.Plot}}</li>
+            <li class="list-group-item"><button type="button" class="btn btn-success" (click)="showTorrents(movie.Title)">Download</button></li>
         </ul>
-    </div>   
+        <div *ngIf="torrents">
+            <ul class="list-group">
+                <li *ngFor="let torrent of torrents.list" class="list-group-item">
+                    {{torrent.title}}
+                </li>
+            </ul>
+        </div>
+    </div>
     `
 })
 
 export class BubflixMovieCardComponent {
     @Input() movie: Movie;
+    
+    private torrentList = false;    
+    private _torrentService;
+    private torrents;
+    
+    constructor(torrent_service: TorrentService) {
+        this._torrentService = torrent_service;
+    }
+    
+    showTorrents(title: string) {
+        this.torrentList = !this.torrentList; 
+        if(this.torrentList) {
+            this._torrentService.search(title, 'movies').subscribe(data => this.torrents = JSON.parse(data));    
+        }    
+    }  
 }
